376eb36c476cec48ff27d8a663302c8037c3483a
Nikita <latinisnotdead07@gmail.com>
2020-06-28 19:43:11 +0100

    Setup to mock ajax requests

---
+++ spec/javascripts/helpers/test_responses.js
@@ -0,0 +1,3 @@
+var TestResponse = {
+response: '{"response":{"status":"ok","userTier":"developer","total":17220,"startIndex":1,"pageSize":10,"currentPage":1,"pages":1722,"orderBy":"relevance","results":[{"id":"politics/2020/jun/23/rees-mogg-loses-vote-to-let-mps-debate-bullying-claims","type":"article","sectionId":"politics","sectionName":"Politics","webPublicationDate":"2020-06-23T20:58:08Z","webTitle":"Rees-Mogg loses vote to let MPs debate bullying claims","webUrl":"https://www.theguardian.com/politics/2020/jun/23/rees-mogg-loses-vote-to-let-mps-debate-bullying-claims","apiUrl":"https://content.guardianapis.com/politics/2020/jun/23/rees-mogg-loses-vote-to-let-mps-debate-bullying-claims","isHosted":false,"pillarId":"pillar/news","pillarName":"News"}'
+}
