054039013f10eb744276709a0d2be182c3b064dd
Nikita <latinisnotdead07@gmail.com>
2020-06-28 17:30:43 +0100

    wraps the article in html element

--- spec/article_list_view_spec.js
+++ spec/article_list_view_spec.js
@@ -1,9 +1,28 @@
 describe("ArticleListView", function() {
 
+  function Article() {
+    this.headline = "some headline"
+    this.url = "www.url.com"
+  }
+
+  var article = new Article
+
   it ("instatiates with an article list", function() {
     var list = new ArticleList;
     var listView = new ArticleListView(list);
-    expect(listView.list.length).toEqual(10)
+    expect(listView.list).toBeInstanceOf(ArticleList)
+  });
+
+  it ("wraps the article list in html elements", function() {
+    var list = new ArticleList;
+    list.list = [];
+    list.list.push(article)
+
+    var listView = new ArticleListView(list);
+    
+    var html = '<div><a href="#article/some headline">some headline</a></div><br>'
+   
+    expect(listView.htmlWrap()).toEqual(html)
   });
 
 });
--- src/article_list_view.js
+++ src/article_list_view.js
@@ -1,7 +1,20 @@
 (function(exports) {
   function ArticleListView(articleList) {
-    this.list = articleList.list;
+    this.list = articleList;
   }
+
+  ArticleListView.prototype.htmlWrap = function() {
+    
+    console.log(this.list.returnList())
+    html_array = []
+    var articles = this.list.returnList();
+    if (articles.length >= 1) {
+      articles.forEach( article => {
+        html_array.push(`<div><a href="#article/${article.headline}">${article.headline}</a></div><br>`)
+      });
+    };
+    return html_array.join("")
+  };
 
   exports.ArticleListView = ArticleListView;
 })(this);
